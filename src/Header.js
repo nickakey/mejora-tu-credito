3d83d97dc770230a56c116dd3d9ddf7fc7f48ba0
Nick Akey <nicktheflanders@gmail.com>
2022-07-28 15:24:32 -0500

    really ugly home page

--- src/Header.js
+++ src/Header.js
@@ -3,13 +3,42 @@ import logo from "./logo.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import { AppBar, Typography, Container, Toolbar, Box, IconButton, MenuIcon, Menu } from "@mui/material";
+import CreditCard from "@mui/icons-material/CreditCard";
+
 
 const Header = () => {
 
     return (
-        <div >
-          THIS IS THE HEADER
-        </div>
+        <>
+            <AppBar position="static">
+                <Container maxWidth="xl">
+                    <Toolbar disableGutters>
+                        <CreditCard
+                            sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+                        />
+                        <Typography
+                            variant="h6"
+                            noWrap
+                            component="a"
+                            href="/"
+                            sx={{
+                                mr: 2,
+                                display: { xs: "none", md: "flex" },
+                                // fontFamily: "monospace",
+                                fontWeight: 700,
+                                // letterSpacing: ".3rem",
+                                color: "inherit",
+                                textDecoration: "none",
+                            }}
+                        >
+                            MejoraTuCredito.io
+                        </Typography>
+
+                    </Toolbar>
+                </Container>
+            </AppBar>
+        </>
     );
 };
 
--- src/HomePage.js
+++ src/HomePage.js
@@ -3,15 +3,92 @@ import logo from "./logo.svg";
 import "./App.css";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import { Card, CardActions, CardContent, Paper, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import CreditScoreIcon from "@mui/icons-material/CreditScore";
+import HistoryEduIcon from "@mui/icons-material/HistoryEdu";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 
-class HomePage extends Component {
-    render() {
+
+const HomePage = ({pageIndex, setPageIndex}) => {
+
+
         return (
-            <div className="home">
-                  THIS IS THE HOMEPAGE
-            </div>
+            <>
+                <Box className="hero-text-homepage">
+                    <Typography variant="h2">
+                        Como Mejorar tu credito
+                    </Typography>
+                </Box>
+                <Paper className="homescreen-paper" elevation={3}>
+                    <Typography variant="body1">
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elit, sed do eiusmod tempor incididunt ut labore et
+                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                        ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu
+                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                        non proident, sunt in culpa qui officia deserunt mollit
+                        anim id est laborum."
+                    </Typography>
+                    <Box className="card-container">
+                        <Card1 />
+                        <Card2 />
+                        <Card3 />
+                    </Box>
+                    <Button onClick={() => setPageIndex(pageIndex + 1)} variant="contained">Mejoralo Ahora!</Button>
+                </Paper>
+            </>
         );
-    }
 }
+
+const Card1 = () => (
+    <Card className="card">
+        <CardContent>
+            <Typography sx={{ fontSize: 18 }} color="white" gutterBottom>
+                Step 1
+            </Typography>
+            <HistoryEduIcon sx={{ fontSize: 60 }} />
+            <Typography variant="body2" sx={{ fontSize: 18 }}>
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+            </Typography>
+        </CardContent>
+    </Card>
+);
+
+const Card2 = () => (
+    <Card className="card">
+        <CardContent>
+            <Typography sx={{ fontSize: 18 }} color="white" gutterBottom>
+                Step 1
+            </Typography>
+            <CreditScoreIcon sx={{ fontSize: 60 }} />
+            <Typography variant="body2" sx={{ fontSize: 18 }}>
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+            </Typography>
+        </CardContent>
+    </Card>
+);
+
+const Card3 = () => (
+    <Card className="card">
+        <CardContent>
+            <Typography sx={{ fontSize: 18 }} color="white" gutterBottom>
+                Step 1
+            </Typography>
+            <EmojiEmotionsIcon sx={{ fontSize: 60 }} />
+            <Typography variant="body2" sx={{ fontSize: 18 }}>
+                well meaning and kindly.
+                <br />
+                {'"a benevolent smile"'}
+            </Typography>
+        </CardContent>
+    </Card>
+);
 
 export default HomePage;
